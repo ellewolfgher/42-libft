450ed7a3ca5b5ca523671d58843f9d974f7e5c1a
Elle Wolfgher <ellewolfgher@gmail.com>
2022-09-04 16:05:08 -0300

    ft_memcmp UNTESTED

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: elle <elle@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 23:40:46 by ewolfghe          #+#    #+#             */
-/*   Updated: 2022/09/04 15:36:10 by elle             ###   ########.fr       */
+/*   Updated: 2022/09/04 16:04:19 by elle             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,6 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
