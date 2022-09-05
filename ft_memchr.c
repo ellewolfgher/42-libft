d99a09f1e855404f9879b7e2bdd99e42b13de18d
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-05 22:08:58 +0200

    ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 17:01:39 by ewolfghe          #+#    #+#             */
+/*   Updated: 2022/09/05 17:11:43 by ewolfghe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	*str;
 	unsigned char	chr;
-	size_t i;
+	size_t			i;
 
 	str = (unsigned char *)s;
 	chr = (unsigned char)c;
@@ -12,7 +24,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (str[i] == chr)
-			return ((void*)(s + i));
+			return ((void *)(s + i));
 		i++;
 	}
 	return (NULL);
