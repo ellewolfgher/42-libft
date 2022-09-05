3933d17d5e807a2dc8b9cccb405c19a548049d8f
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-05 22:07:32 +0200

    ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 17:03:42 by ewolfghe          #+#    #+#             */
+/*   Updated: 2022/09/05 17:38:11 by ewolfghe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 int	ft_atoi(const char *nptr)
@@ -8,7 +20,7 @@ int	ft_atoi(const char *nptr)
 
 	neg = 1;
 	i = 0;
-	while (nptr[i] <= 32 || nptr[i] == 127)
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
 	if (nptr[i] == '+' || nptr[i] == '-')
 	{
@@ -16,7 +28,6 @@ int	ft_atoi(const char *nptr)
 			neg = -1;
 		i++;
 	}
-
 	res = 0;
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
@@ -24,5 +35,5 @@ int	ft_atoi(const char *nptr)
 		res = res + (nptr[i] - 48);
 		i++;
 	}
-	return (res * neg);		
+	return (res * neg);
 }
