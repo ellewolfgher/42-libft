a93bf2a6c9140768c8477063bd1fbd4c6d7483e9
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-05 22:51:33 +0200

    itoa WRONG

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 17:15:28 by ewolfghe          #+#    #+#             */
+/*   Updated: 2022/09/05 22:27:49 by ewolfghe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_itoa(int n)
 {
 	int		i;
-	int 	aux;
+	int		aux;
 	char	*chr;
 
 	aux = n;
@@ -16,11 +28,15 @@ char	*ft_itoa(int n)
 	chr = malloc(i + 1);
 	chr[i] = '\0';
 	i--;
+	if (n < 0)
+	{
+		chr[i] = '-';
+		ft_itoa(n * -1);
+	}
 	while (i >= 0)
 	{
 		chr[i] = aux % 10 + 48;
 		aux = aux / 10;
 		i--;
 	}
-	return (chr);
 }
