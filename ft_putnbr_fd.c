10cc6e174862da617d8301946c6c00df735efb13
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-05 22:11:12 +0200

    ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,23 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/05 17:16:15 by ewolfghe          #+#    #+#             */
+/*   Updated: 2022/09/05 22:03:59 by ewolfghe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	unsigned int	nbr;
-
 	if (n == -2147483648)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putnbr_fd(214748364, fd);
 		ft_putchar_fd('8', fd);
 	}
-	if (n < 0)
+	else if (n >= 0 && n <= 9)
+		ft_putchar_fd((n + 48), fd);
+	else if (n >= 10)
 	{
-		ft_putchar_fd('-', fd);
-		nbr = (unsigned int)(n * -1);
+		ft_putnbr_fd((n / 10), fd);
+		ft_putchar_fd((n % 10 + 48), fd);
 	}
 	else
-		nbr = (unsigned int)n;
-	if (nbr >= 10)
-		ft_putnbr_fd(nbr / 10, fd);
-	ft_putchar_fd((char)(nbr % 10 + 48), fd);
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd((n * -1), fd);
+	}
 }
