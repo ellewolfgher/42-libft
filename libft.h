fe8467b2e53333c5a225b2da8b7cf2c495d814a4
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-08 00:46:02 +0200

    ft_striteri

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 23:40:46 by ewolfghe          #+#    #+#             */
-/*   Updated: 2022/09/07 21:12:18 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2022/09/08 00:11:43 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,5 +44,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *lil, size_t len);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif
