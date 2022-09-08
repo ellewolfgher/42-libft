b1ff4421a6517f3fcbd79622f9420bc21d433e83
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-09 01:26:44 +0200

    ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,32 +6,11 @@
 /*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 00:52:11 by ewolfghe          #+#    #+#             */
-/*   Updated: 2022/09/09 01:24:37 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2022/09/09 01:26:21 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-/* char	*ft_strtrim(char const *s1, char const *set)
-{
-	size_t	start;
-	size_t	end;
-	char	*str;
-
-	start = 0;
-	end = ft_strlen(s1);
-	if (!s1 || !set)
-		return (NULL);
-	while (s1[start] && ft_strchr(set, s1[start]))
-			start++;
-	while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > start)
-			end--;
-	str = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, &s1[start], end - start + 1);
-	return (str);
-} */
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -51,23 +30,3 @@ char	*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(str, &s1[i], j - i + 1);
 	return (str);
 }
-
-/*
-Copia `s1` removendo caracteres localizados em `set`.
-
-Retorna a string criada ou nulo caso a alocação falhe.
-
-	start = 0;
-	end = ft_strlen(s1);
-	if (!s1 || !set)
-		return (NULL);
-	while (s1[start] && ft_strchr(set, s1[start]))
-			start++;
-	while (s1[end - 1] && ft_strchr(set, s1[end - 1]) && end > start)
-			end--;
-	str = (char *)malloc(sizeof(char) * (end - start + 1));
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, &s1[start], end - start + 1);
-	return (str);
-*/
