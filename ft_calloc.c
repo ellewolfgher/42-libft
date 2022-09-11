56746a81435b2f5b097f7e954accfaf1f6ad390c
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-12 00:13:32 +0200

    ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 17:05:30 by ewolfghe          #+#    #+#             */
-/*   Updated: 2022/09/05 17:05:32 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2022/09/12 00:11:38 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	if (!ptr || !nmemb || !size || size > __SIZE_MAX__ / nmemb)
 		return (NULL);
 	ft_bzero(ptr, (nmemb * size));
 	return (ptr);
