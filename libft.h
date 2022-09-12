c4ad40efc049f447a8455168d972f615b9a2030b
Elle Wolfgher <ewolfghe@student.42sp.org.br>
2022-09-13 01:10:39 +0200

    bonus lstdelone

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ewolfghe <ewolfghe@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 23:40:46 by ewolfghe          #+#    #+#             */
-/*   Updated: 2022/09/13 00:53:56 by ewolfghe         ###   ########.fr       */
+/*   Updated: 2022/09/13 01:02:46 by ewolfghe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,7 @@ t_list	*ft_lstnew(void *content);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 int		ft_lstsize(t_list *lst);
 
 #endif
